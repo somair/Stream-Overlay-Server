54db513fabffa5a670ddb730b80b2a7a93124901
Allister <allisterkmoon@gmail.com>
2017-05-23 23:19:10 -0700

    Further setup of server
    Wrapped things in a venv instance and set a requirements.txt
    Got the first part of the Twitch API working correctly, it sends a
    response to the api_redirect url with the code. But I'm currently stuck
    on the next point, turning that into an access_token. Work to be
    continued!

--- app/overlay/urls.py
+++ app/overlay/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^api_redirect', views.api_redirect, name='api_redirect')
 ]
--- app/overlay/views.py
+++ app/overlay/views.py
@@ -1,11 +1,47 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from os import environ
 import requests
 
 def index(request):
-    # Currently using an alternative API to get twitch info with authentication
-    # Need to figure out how to authenticate with the proper API
-    response = requests.get("https://wind-bow.gomix.me/twitch-api/channels/alyxdelunar/follows")
-    context = {
-        'data': response.json()
-    }
-    return render(request, 'overlay/index.html', context)
+    url = 'https://api.twitch.tv/kraken/oauth2/authorize'
+    url = url + '?client_id=' + environ.get('client_id')
+    url = url + '&redirect_uri=http://localhost:8000/overlay/api_redirect'
+    url = url + '&scope=channel_subscriptions+viewing_activity_read'
+    url = url + '&response_type=code'
+    return redirect(url)
+
+def api_redirect(request):
+    print('---------------------- IN REDIRECT --------------------')
+    print(request.POST)
+    if request.GET.get('access_token', None) is None:
+        data = {
+            'client_id': environ.get('client_id'),
+            'client_secret': environ.get('client_secret'),
+            'code': request.GET.get('code'),
+            'grant_type': 'authorization_code',
+            'redirect_uri': 'http://localhost:8000/overlay/api_redirect',
+            'state': request.GET.get('state')
+        }
+        requests.post('https://api.twitch.tv/kraken/token', data=data)
+
+        context = {}
+        return render(request, 'overlay/index.html', context)
+    return redirect('https://www.google.com')
+
+# def api_redirect(request):
+#     print('---------------------- IN REDIRECT --------------------')
+#     if request.GET.get('access_token', None) is None:
+#
+#         url = 'https://api.twitch.tv/kraken/oauth2/token'
+#         url = url + '?client_id=' + environ.get('client_id')
+#         url = url + '&client_secret=' + environ.get('client_secret')
+#         url = url + '&code=' + request.GET.get('code')
+#         url = url + '&grant_type=authorization_code'
+#         url = url + '&redirect_uri=http://localhost:8000/overlay/api_redirect'
+#         print(url)
+#         return redirect(url)
+#     else:
+#         print(request)
+#         context = {}
+#         return render(request, 'overlay/index.html', context)
